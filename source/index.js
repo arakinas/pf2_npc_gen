36856a219b9e92ee173c34417dfcc35cf58469ea
Landon Montgomery <arakinas@hotmail.com>
2020-11-02 17:06:54 -0600

    Partially implemented ability generation. Current issue found with background sometimes selecting the same stat twice. Committing prior to refactor

--- source/index.js
+++ source/index.js
@@ -68,7 +68,6 @@ function GenerateLevels() {
 function ClearResults() {
   testResultsList.value = "";
   console.log("clear Results clicked");
-  //Current Bug! After clearing results the textArea won't populate again.
 }
 
 function GenerateNpc() {
@@ -90,16 +89,34 @@ function GenerateNpc() {
   let npcLevel = npcLevelSelector.value;
   let npcAncestery = npcAncesterySelector.value;
 
-  if(npcAncesterySelector.selectedIndex < 2){
-      npcAncestery = "Random";
+  if (npcAncesterySelector.selectedIndex < 2) {
+    npcAncestery = "Random";
   }
 
   let newNpc = new CreateNpc(npcClass, npcLevel, npcAncestery);
 
   addResultWithLine("Creating New Npc:");
-  addResultWithLine(GetNpcName() + ", the level " + newNpc.npcLevel + 
-  " " + newNpc.npcAncestery +  " " + newNpc.npcClass + "!");
+  addResultWithLine(
+    newNpc.npcName +
+      ", the level " +
+      newNpc.npcLevel +
+      " " +
+      newNpc.npcAncestery +
+      " " +
+      newNpc.npcClass +
+      "!"
+  );
+  addResultWithLine("Background: " + newNpc.npcBackGround[0]);
+  LogStats(
+    newNpc.charisma,
+    newNpc.constitution,
+    newNpc.dexterity,
+    newNpc.intelligence,
+    newNpc.strength,
+    newNpc.wisdom
+  );
 
+  console.log(newNpc.npcName + " generated");
 }
 
 function ResetSelections() {
@@ -121,35 +138,720 @@ document
 document.onload = PopulateDropDowns();
 
 function addResultWithLine(textToAdd) {
-  testResultsList.append(textToAdd + "\n");
+  testResultsList.value += textToAdd + "\n";
+}
+
+//Test Method
+function LogStats(
+  charisma,
+  constitution,
+  dexterity,
+  intelligence,
+  strength,
+  wisdom
+) {
+  addResultWithLine(
+    "Strength:" +
+      strength +
+      ", Dexterity:" +
+      dexterity +
+      ", Constitution:" +
+      constitution
+  );
+  addResultWithLine(
+    "Intelligence:" +
+      intelligence +
+      " Wisdom:" +
+      wisdom +
+      " Charisma:" +
+      charisma
+  );
+  //debug total
+  addResultWithLine(
+    "Total stats = " +
+      (strength + dexterity + constitution + intelligence + wisdom + charisma)
+  );
 }
 
 class CreateNpc {
   constructor(npcClass, npcLevel, npcAncestery) {
+    if (npcAncestery == "Random" || npcAncestery == "Select Ancestery") {
+      let ran = Math.floor(Math.random() * ancesteryTypes.length);
 
+      npcAncestery = ancesteryTypes[ran];
 
-    if ((npcAncestery == "Random") || (npcAncestery == "Select Ancestery")) {
-        let ran = Math.floor(Math.random() * (ancesteryTypes.length));
-    
-        npcAncestery = ancesteryTypes[ran];
+      //// Need to add stat adjustments for the ancestry
+    }
+
+    if (npcClass == "Random" || npcClass == "Select Type") {
+      let ran = Math.floor(Math.random() * ancesteryTypes.length);
+
+      npcClass = classTypes[ran];
+    }
+
+    if (npcLevel == "Random" || npcLevel == "Select Level") {
+      let levels = GenerateLevels();
+      let ran = Math.floor(Math.random() * levels.length);
+
+      npcLevel = levels[ran];
+    }
+
+    let charisma = 10;
+    let constitution = 10;
+    let dexterity = 10;
+    let intelligence = 10;
+    let strength = 10;
+    let wisdom = 10;
+
+    //Initializing this value, just as a precaution
+    let primaryStat = "Constitution";
+    if (
+      npcClass == "Alchemist" ||
+      npcClass == "Investigator" ||
+      npcClass == "Witch" ||
+      npcClass == "Wizard"
+    ) {
+      primaryStat = "Intelligence";
+      intelligence += 2;
+    } else if (
+      npcClass == "Bard" ||
+      npcClass == "Sorcerer" ||
+      npcClass == "Oracle"
+    ) {
+      primaryStat = "Charisma";
+      charisma += 2;
+    } else if (npcClass == "Druid" || npcClass == "Cleric") {
+      primaryStat = "Wisdom";
+      wisdom += 2;
+    } else if (
+      npcClass == "Ranger" ||
+      npcClass == "Rogue" ||
+      npcClass == "Swashbuckler" ||
+      npcClass == "Champion"
+    ) {
+      primaryStat = "Dexterity";
+      dexterity += 2;
+    } else if (
+      npcClass == "Monk" ||
+      npcClass == "Fighter" ||
+      npcClass == "Barbarian"
+    ) {
+      primaryStat = "Strength";
+      strength += 2;
+    }
+
+    if (primaryStat == "Constitution") {
+      constitution += 2;
+    }
+
+    let npcBackground = GetBackground(primaryStat);
+    this.npcBackGround = npcBackground;
+
+    let npcAncesteryStats = GetAncestoryStats(npcAncestery, primaryStat);
+    //[charisma, constitution, dexterity, intelligence, strength, wisdom]
+    charisma += npcAncesteryStats[0];
+    constitution += npcAncesteryStats[1];
+    dexterity += npcAncesteryStats[2];
+    intelligence += npcAncesteryStats[3];
+    strength += npcAncesteryStats[4];
+    wisdom += npcAncesteryStats[5];
+
+    if (this.npcBackGround[1] == "Charisma") {
+      charisma += 2;
+    } else if (this.npcBackGround[1] == "Constitution") {
+      constitution += 2;
+    } else if (this.npcBackGround[1] == "Dexterity") {
+      dexterity += 2;
+    } else if (this.npcBackGround[1] == "Intelligence") {
+      intelligence += 2;
+    } else if (this.npcBackGround[1] == "Strength") {
+      strength += 2;
+    } else if (this.npcBackGround[1] == "Wisdom") {
+      wisdom += 2;
+    } else {
+      console.log("Error in background primary stat: " + this.npcBackGround[1]);
+    }
+
+    if (this.npcBackGround[2] == "Charisma") {
+      charisma += 2;
+    } else if (this.npcBackGround[2] == "Constitution") {
+      constitution += 2;
+    } else if (this.npcBackGround[2] == "Dexterity") {
+      dexterity += 2;
+    } else if (this.npcBackGround[2] == "Intelligence") {
+      intelligence += 2;
+    } else if (this.npcBackGround[2] == "Strength") {
+      strength += 2;
+    } else if (this.npcBackGround[2] == "Wisdom") {
+      wisdom += 2;
+    } else {
+      console.log(
+        "Error in background secondary stat: " + this.npcBackGround[2]
+      );
+    }
+
+    //Four free ability boosts, for last step.
+    if (
+      npcClass == "Alchemist" ||
+      npcClass == "Witch" ||
+      npcClass == "Wizard"
+    ) {
+      intelligence += 2;
+      dexterity += 2;
+      constitution += 2;
+      wisdom += 2;
+    } else if (npcClass == "Bard" || npcClass == "Sorcerer") {
+      intelligence += 2;
+      charisma += 2;
+      dexterity += 2;
+      wisdom += 2;
+    } else if (
+      npcClass == "Druid" ||
+      npcClass == "Cleric" ||
+      npcClass == "Oracle"
+    ) {
+      intelligence += 2;
+      charisma += 2;
+      strength += 2;
+      wisdom += 2;
+    } else if (
+      npcClass == "Ranger" ||
+      npcClass == "Swashbuckler" ||
+      npcClass == "Champion"
+    ) {
+      strength += 2;
+      dexterity += 2;
+      charisma += 2;
+      wisdom += 2;
+    } else if (
+      npcClass == "Monk" ||
+      npcClass == "Fighter" ||
+      npcClass == "Barbarian"
+    ) {
+      strength += 2;
+      dexterity += 2;
+      constitution += 2;
+      wisdom += 2;
+    } else if (npcClass == "Investigator" || npcClass == "Rogue") {
+      intelligence += 2;
+      dexterity += 2;
+      strength += 2;
+      wisdom += 2;
+    }
+
+    this.charisma = charisma;
+    this.constitution = constitution;
+    this.dexterity = dexterity;
+    this.intelligence = intelligence;
+    this.strength = strength;
+    this.wisdom = wisdom;
+    this.npcClass = npcClass;
+    this.npcLevel = npcLevel;
+    this.npcAncestery = npcAncestery;
+    this.npcName = GetNpcName();
+  }
+}
+
+//Generates Stats for the ancestery
+//this contains positives and negatives, so an array will be returned
+//stats in alphabetical order as a value set
+function GetAncestoryStats(npcAncestery, primaryStat) {
+  let charisma = 0;
+  let constitution = 0;
+  let dexterity = 0;
+  let intelligence = 0;
+  let strength = 0;
+  let wisdom = 0;
+
+  switch (npcAncestery) {
+    case "Dwarf":
+      constitution += 2;
+      wisdom += 2;
+      charisma -= 2;
+      break;
+    case "Elf":
+      dexterity += 2;
+      intelligence += 2;
+      constitution -= 2;
+      break;
+    case "Gnome":
+      constitution += 2;
+      charisma += 2;
+      strength -= 2;
+      break;
+    case "Goblin":
+      dexterity += 2;
+      charisma += 2;
+      wisdom -= 2;
+      break;
+    case "Halfling":
+      dexterity += 2;
+      wisdom += 2;
+      strength -= 2;
+      break;
+    case "Catfolk":
+      dexterity += 2;
+      charisma += 2;
+      wisdom -= 2;
+      break;
+    case "Kobold":
+      dexterity += 2;
+      charisma += 2;
+      constitution -= 2;
+      break;
+    case "Orc":
+      strength += 2;
+      break;
+    case "Ratfolk":
+      dexterity += 2;
+      intelligence += 2;
+      strength -= 2;
+      break;
+    case "Tengu":
+      dexterity += 2;
+      break;
+    case "Leshy":
+      constitution += 2;
+      wisdom += 2;
+      intelligence -= 2;
+      break;
+    case "Lizardfolk":
+      strength += 2;
+      wisdom += 2;
+      intelligence -= 2;
+      break;
+    case "Hobgoblin":
+      constitution += 2;
+      intelligence += 2;
+      wisdom -= 2;
+      break;
+    case "Shoony":
+      dexterity += 2;
+      charisma += 2;
+      constitution -= 2;
+      break;
+  }
+//setting this before getting the free ability to help track selected boosts
+  let ancesteryStats = [
+    charisma,
+    constitution,
+    dexterity,
+    intelligence,
+    strength,
+    wisdom,
+  ];
+
+  ancesteryStats = selectAbilityBoost(ancesteryStats, primaryStat);
+
+  console.log("ancestry stats: " + ancesteryStats);
+  return ancesteryStats;
+}
+
+//selects an ability to boost. Removes items already selected from the options. 
+//Prefers primary stat.
+function selectAbilityBoost(npcStats, primaryStat){
+ let charisma = npcStats[0];
+  let constitution = npcStats[1];
+  let dexterity = npcStats[2];
+  let intelligence = npcStats[3];
+  let strength = npcStats[4];
+  let wisdom = npcStats[5];
+
+let statList = ["Charisma","Constitution","Dexterity","Intelligence","Strength","Wisdom"]
+
+if(charisma > 0){statList.slice(0,1);}
+if(constitution > 0){statList.slice(0,1);}
+if(dexterity > 0){statList.slice(0,1);}
+if(intelligence > 0){statList.slice(0,1);}
+if(strength > 0){statList.slice(0,1);}
+if(wisdom > 0){statList.slice(0,1);}
+
+let primaryIndex = statList.indexOf(primaryStat);
+
+let selectedStat = "";
+
+if(primaryIndex > -1){
+  selectedStat = statList[primaryIndex];
+}
+else{
+  selectedStat = statList[Math.floor(Math.random() * statList.length)];
+}
+
+switch(selectedStat){
+  case "Charisma":
+    charisma+=2;
+    break;
+    case "Constitution":
+      constitution+=2;
+    break;
+    case "Dexterity":
+      dexterity+=2;
+    break;
+    case "Intelligence":
+      intelligence+=2;
+    break;
+    case "Strength":
+      strength+=2;
+    break;
+    case "Wisdom":
+      wisdom+=2;
+      break;
+}
+
+let adjustedStats = [
+  charisma,
+  constitution,
+  dexterity,
+  intelligence,
+  strength,
+  wisdom,
+];
+
+  return adjustedStats;
+}
+
+//Generates a random background for the npc
+//Primary stat is pulled in to help select stat selection
+function GetBackground(primaryStat) {
+  let firstStat = "";
+  let secondStat = "";
+
+  let backgrounds = [
+    "Acrobat",
+    "Acolyte",
+    "Animal Whisperer",
+    "Artisan",
+    "Artist",
+    "Barkeep",
+    "Barrister",
+    "Bounty Hunter",
+    "Charlatan",
+    "Criminal",
+    "Detective",
+    "Emissary",
+    "Entertainer",
+    "Farmhand",
+    "Field Medic",
+    "Fortune Teller",
+    "Gambler",
+    "Gladiator",
+    "Guard",
+    "Herbalist",
+    "Hermit",
+    "Hunter",
+    "Laborer",
+    "Martial Disciple",
+    "Merchant",
+    "Miner",
+    "Noble",
+    "Nomad",
+    "Prisoner",
+    "Sailor",
+    "Scholar",
+    "Scout",
+    "Street Urchin",
+    "Tinker",
+    "Warrior",
+  ];
+
+  let background = backgrounds[Math.floor(Math.random() * backgrounds.length)];
+  let firstStatIndex = Math.floor(Math.random());
+
+  console.log("background " + background);
+  console.log("First stat index:" + firstStatIndex);
+
+  switch (background) {
+    case "Acolyte":
+      if (firstStatIndex == 1) {
+        firstStat = "Wisdom";
+      } else {
+        firstStat = "Intelligence";
       }
-
-      if ((npcClass == "Random") || (npcClass == "Select Type")) {
-        let ran = Math.floor(Math.random() * (ancesteryTypes.length));
-    
-        npcClass = classTypes[ran];
+      break;
+    case "Acrobat":
+      if (firstStatIndex == 1) {
+        firstStat = "Strength";
+      } else {
+        firstStat = "Dexterity";
       }
-
-      if ((npcLevel == "Random") || (npcLevel == "Select Level")){
-          let levels = GenerateLevels();
-        let ran = Math.floor(Math.random() * (levels.length));
-    
-        npcLevel = levels[ran];
+      break;
+    case "Animal Whisperer":
+      if (firstStatIndex == 1) {
+        firstStat = "Wisdom";
+      } else {
+        firstStat = "Charisma";
       }
+      break;
+    case "Artisan":
+      if (firstStatIndex == 1) {
+        firstStat = "Strength";
+      } else {
+        firstStat = "Intelligence";
+      }
+      break;
+    case "Artist":
+      if (firstStatIndex == 1) {
+        firstStat = "Dexterity";
+      } else {
+        firstStat = "Charisma";
+      }
+      break;
+    case "Barkeep":
+      if (firstStatIndex == 1) {
+        firstStat = "Constitution";
+      } else {
+        firstStat = "Charisma";
+      }
+      break;
+    case "Barrister":
+      if (firstStatIndex == 1) {
+        firstStat = "Charisma";
+      } else {
+        firstStat = "Intelligence";
+      }
+      break;
+    case "Bounty Hunter":
+      if (firstStatIndex == 1) {
+        firstStat = "Wisdom";
+      } else {
+        firstStat = "Strength";
+      }
+      break;
+    case "Charlatan":
+      if (firstStatIndex == 1) {
+        firstStat = "Charisma";
+      } else {
+        firstStat = "Intelligence";
+      }
+      break;
+    case "Criminal":
+      if (firstStatIndex == 1) {
+        firstStat = "Dexterity";
+      } else {
+        firstStat = "Intelligence";
+      }
+      break;
+    case "Detective":
+      if (firstStatIndex == 1) {
+        firstStat = "Wisdom";
+      } else {
+        firstStat = "Intelligence";
+      }
+      break;
+    case "Emissary":
+      if (firstStatIndex == 1) {
+        firstStat = "Charisma";
+      } else {
+        firstStat = "Intelligence";
+      }
+      break;
+    case "Entertainer":
+      if (firstStatIndex == 1) {
+        firstStat = "Charisma";
+      } else {
+        firstStat = "Dexterity";
+      }
+      break;
+    case "Farmhand":
+      if (firstStatIndex == 1) {
+        firstStat = "Wisdom";
+      } else {
+        firstStat = "Constitution";
+      }
+      break;
+    case "Field Medic":
+      if (firstStatIndex == 1) {
+        firstStat = "Wisdom";
+      } else {
+        firstStat = "Constitution";
+      }
+      break;
+    case "Fortune Teller":
+      if (firstStatIndex == 1) {
+        firstStat = "Charisma";
+      } else {
+        firstStat = "Intelligence";
+      }
+      break;
+    case "Gambler":
+      if (firstStatIndex == 1) {
+        firstStat = "Dexterity";
+      } else {
+        firstStat = "Charisma";
+      }
+      break;
+    case "Gladiator":
+      if (firstStatIndex == 1) {
+        firstStat = "Strength";
+      } else {
+        firstStat = "Charisma";
+      }
+      break;
+    case "Guard":
+      if (firstStatIndex == 1) {
+        firstStat = "Strength";
+      } else {
+        firstStat = "Charisma";
+      }
+      break;
+    case "Herbalist":
+      if (firstStatIndex == 1) {
+        firstStat = "Wisdom";
+      } else {
+        firstStat = "Constitution";
+      }
+      break;
+    case "Hermit":
+      if (firstStatIndex == 1) {
+        firstStat = "Constitution";
+      } else {
+        firstStat = "Intelligence";
+      }
+      break;
+    case "Hunter":
+      if (firstStatIndex == 1) {
+        firstStat = "Wisdom";
+      } else {
+        firstStat = "Dexterity";
+      }
+      break;
+    case "Laborer":
+      if (firstStatIndex == 1) {
+        firstStat = "Strength";
+      } else {
+        firstStat = "Constitution";
+      }
+      break;
+    case "Martial Disciple":
+      if (firstStatIndex == 1) {
+        firstStat = "Strength";
+      } else {
+        firstStat = "Dexterity";
+      }
+      break;
+    case "Merchant":
+      if (firstStatIndex == 1) {
+        firstStat = "Charisma";
+      } else {
+        firstStat = "Intelligence";
+      }
+      break;
+    case "Miner":
+      if (firstStatIndex == 1) {
+        firstStat = "Wisdom";
+      } else {
+        firstStat = "Strength";
+      }
+      break;
+    case "Noble":
+      if (firstStatIndex == 1) {
+        firstStat = "Wisdom";
+      } else {
+        firstStat = "Intelligence";
+      }
+      break;
+    case "Nomad":
+      if (firstStatIndex == 1) {
+        firstStat = "Wisdom";
+      } else {
+        firstStat = "Constitution";
+      }
+      break;
+    case "Prisoner":
+      if (firstStatIndex == 1) {
+        firstStat = "Strength";
+      } else {
+        firstStat = "Constitution";
+      }
+      break;
+    case "Sailor":
+      if (firstStatIndex == 1) {
+        firstStat = "Strength";
+      } else {
+        firstStat = "Dexterity";
+      }
+      break;
+    case "Scholar":
+      if (firstStatIndex == 1) {
+        firstStat = "Wisdom";
+      } else {
+        firstStat = "Intelligence";
+      }
+      break;
+    case "Scout":
+      if (firstStatIndex == 1) {
+        firstStat = "Wisdom";
+      } else {
+        firstStat = "Dexterity";
+      }
+      break;
+    case "Street Urchin":
+      if (firstStatIndex == 1) {
+        firstStat = "Constitution";
+      } else {
+        firstStat = "Dexterity";
+      }
+      break;
+    case "Tinker":
+      if (firstStatIndex == 1) {
+        firstStat = "Dexterity";
+      } else {
+        firstStat = "Intelligence";
+      }
+      break;
+    case "Warrior":
+      if (firstStatIndex == 1) {
+        firstStat = "Strength";
+      } else {
+        firstStat = "Constitution";
+      }
+      break;
+  }
 
-      this.npcClass = npcClass;
-      this.npcLevel = npcLevel;
-      this.npcAncestery = npcAncestery;
+  console.log("First Stat: " + firstStat);
+
+  if (firstStat != primaryStat) {
+    secondStat = primaryStat;
+
+    //This bombs out if I don't have the value assigned to an array before returning.
+    let _background = [background, firstStat, secondStat];
+
+    return _background;
+  } else {
+    console.log("second stat is not primary");
+
+    //Remove the first stat from the list,
+    //you cannot select the same stat to raise at the same step twice.
+    let secondStatList = [
+      "Charisma",
+      "Constitution",
+      "Dexterity",
+      "Intelligence",
+      "Strength",
+      "Wisdom",
+    ];
+    secondStatList.slice((secondStatList.indexOf(firstStat), 1));
+
+    secondStat = Math.floor(Math.random() * secondStatList.length);
+
+    console.log("second stat " + secondStat);
+
+    if (background == null || firstStat == null || secondStat == null) {
+      console.log("error with background");
+    }
+    if (
+      background == undefined ||
+      firstStat == undefined ||
+      secondStat == undefined
+    ) {
+      console.log("error with background");
+    }
+
+    console.log("Background:" + background);
+    console.log("firstStat: " + firstStat);
+    console.log("secondStat: " + secondStat);
+
+    return [background, firstStat, secondStat];
   }
 }
 
